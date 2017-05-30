6abd66bfdcb1557a171295a934f4148ba50cf0f1
Markus Wolf <mail@markus-wolf.de>
2017-05-30 10:28:40 +0200

    refactor: update apis to have test run without io

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,35 @@
 import { stripIndent } from 'common-tags';
-import { join } from 'path';
-import transform from './identifiers.test-transform';
+import * as ts from 'typescript';
 import { applyTransforms } from './index';
+import * as types from './types';
 
-test('run identifiers.transform', async() => {
-  const inputPath = join(process.cwd(), 'src', '__fixtures__', 'identifiers.ts');
-  const result = await applyTransforms(inputPath, [transform]);
+test('run identifiers transform', () => {
+  /**
+   * Example tscodeshift transformer. Simply reverses the names of all
+   * identifiers.
+   */
+  function identifiersTransform(file: types.File, api: types.API): string {
+    const t = api.tscodeshift;
 
-  expect(result.trim()).toBe(stripIndent`
-    export function tset(): void {
-      //
+    return t(file.source)
+      .find(ts.SyntaxKind.Identifier)
+      .replaceWith(
+        node => ts.createIdentifier(node.text.split('').reverse().join(''))
+      )
+      .toSource();
+  }
+  const source = stripIndent`
+    export function test(): void {
+      var abc;
     }
-  `);
+  `;
+  const expected = stripIndent`
+    export function tset(): void {
+      var cba;
+    }
+  `;
+
+  const actual = applyTransforms('path.ts', source, [identifiersTransform]);
+
+  expect(actual).toBe(expected);
 });
