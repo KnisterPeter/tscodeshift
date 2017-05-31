99247c8c7b70646ada5c0ff6eb90e33666bd0616
Markus Wolf <mail@markus-wolf.de>
2017-05-31 16:08:40 +0200

    feat: add dry and print options to cli

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import { applyTransforms } from './transform';
 import * as types from './types';
 
 async function main(cli: meow.Result): Promise<void> {
+  if (cli.flags.h) {
+    cli.showHelp(0);
+    return;
+  }
   const transformPaths = [].concat(
     cli.flags['t'] || [],
     cli.flags['transform'] || []
@@ -15,8 +19,12 @@ async function main(cli: meow.Result): Promise<void> {
   paths.forEach(async path => {
     const source = (await fs.readFile(path)).toString();
     const result = applyTransforms(path, source, transforms);
-    if (result !== source) {
+    if (result !== source && !cli.flags.d && !cli.flags.dry) {
       await fs.writeFile(path, result);
+    }
+    if (cli.flags.print || cli.flags.p) {
+      console.log(`Output for ${path}`);
+      console.log(result);
     }
   });
 }
@@ -28,6 +36,8 @@ const cli = meow(`
 
   Options:
     -t FILE, --transform FILE   Path to the transform file. Can be either a local path or url  [./transform.js]
+    -d, --dry                   Dry run (no changes are made to files)
+    -p, --print                 Print output, useful for development
 `);
 
 main(cli)
