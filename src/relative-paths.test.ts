51819aa20b2f97b290bca1fb3b57656588eafe27
brekk <brekk@brekkbockrath.com>
2017-10-04 10:19:22 -0700

    moved test

---
+++ src/relative-paths.test.ts
@@ -0,0 +1,50 @@
+/* global test, expect, afterEach */
+// https://stackoverflow.com/questions/39415661
+import execa = require('execa');
+import * as path from 'path';
+const shell = ((execa as any).shell);
+const cwd = process.cwd();
+const CLI = path.resolve(cwd, `./dist/src/index.js`);
+
+interface Results {
+  stdout: string;
+}
+
+const expectedFixture = `beforeAll(()=> {
+  // something
+});
+
+beforeEach(()=> {
+});
+
+describe('Array', function() {
+  beforeEach(()=> {
+  });
+
+  it('Array', (done: jest.DoneCallback) => {
+    done();
+  });
+
+  it.skip('Array', function(done) {
+    done();
+  });
+});
+
+describe('', () => {
+  it('', (done: jest.DoneCallback) => {
+    done();
+  });
+});`;
+
+test(`tscodeshift should run files relative to the current working directory`, () => {
+  expect.assertions(1);
+  return shell([
+    `cp mocha.fixture.to-copy mocha.fixture.ts`,
+    `node ${CLI} -t ./dist/src/transforms/mocha.js 'mocha.fixture.ts'`,
+    `cat mocha.fixture.ts`
+  ].join(` && `)).then((results: Results) => {
+    expect(results.stdout).toBe(expectedFixture);
+  });
+});
+
+afterEach(() => shell(`rm mocha.fixture.ts`));
